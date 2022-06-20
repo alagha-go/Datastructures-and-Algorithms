f3792fee789ad2d6400abfac82b6a6ab1285cf73
alagha-go <abdihakimosman1480@gmail.com>
2022-06-20 21:54:40 +0300

    added methods to extract and hepifydown

--- lib/heap.go
+++ lib/heap.go
@@ -1,21 +1,66 @@
 package lib
 
+import "fmt"
+
 
 type Heap struct {
 	Slice []int
 }
 
 /// insert value to the heap
-func (h *Heap) Insert(value int) {
+func (h *Heap) InsertMax(value int) {
 	h.Slice = append(h.Slice, value)
-	h.HipifyUp(len(h.Slice)-1)
+	h.MaxHipifyUp(len(h.Slice)-1)
 }
 
 /// heapify heap from bottom to top
-func (h *Heap) HipifyUp(index int) {
+func (h *Heap) MaxHipifyUp(index int) {
 	for h.Slice[Parent(index)] < h.Slice[index] {
 		h.Swap(Parent(index), index)
 		index = Parent(index)
+	}
+}
+
+/// extracts largest value from the heap slice
+func (h *Heap) ExtractMax() int {
+	if len(h.Slice) == 0 {
+		fmt.Println("cannot extract from an empty slice")
+		return -1
+	}
+	value := h.Slice[0]
+	lastindex := len(h.Slice)-1
+	h.Slice[0] = h.Slice[lastindex]
+	h.Slice = h.Slice[:lastindex]
+	return value
+}
+
+// heapify from top to bottom
+func (h *Heap) MaxHipifyDown(index int) {
+	lastindex := len(h.Slice)-1
+	left, right := LeftChild(index), RightChild(index)
+	child := 0
+
+	// loop if the current index has at least one child
+	for left <= lastindex {
+		// when the left child is the only child
+		if left == lastindex {
+			child = left
+		}else if h.Slice[left] > h.Slice[right] {
+			// when the left child is larger than the right child
+			child = left
+		}else {
+			// when the right child is larger than the left child
+			child = right
+		}
+
+		// compare the current index to larger child and swap
+		if h.Slice[index] < h.Slice[child] {
+			h.Swap(index, child)
+			index = child
+			left, right = LeftChild(index), RightChild(index)
+		}else {
+			return
+		}
 	}
 }
 
