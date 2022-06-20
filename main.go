a4fe9042abbf2771f63b8087983e2bb80a4bd515
alagha-go <abdihakimosman1480@gmail.com>
2022-06-20 21:55:04 +0300

    main function for testing

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
+import (
+	"DSAL/lib"
+	"fmt"
+)
+
 
 func main() {
-	
+	var heap lib.Heap
+	slice := []int{10, 20, 30, 21, 43, 19, 28, 31, 67, 47, 86, 91}
+
+	for _, value := range slice {
+		heap.InsertMax(value)
+		fmt.Println(heap.Slice)
+	}
+
+	for index:=0; index<10; index++ {
+		heap.ExtractMax()
+		fmt.Println(heap.Slice)
+	}
+
+	heap.ExtractMax()
+	fmt.Println(heap.Slice)
 }
